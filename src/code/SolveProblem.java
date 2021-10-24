9b54616a63fd98b19a9a1453f0c0651516e514ed
elifyurt <yrtelf@gmail.com>
2021-10-24 22:00:57 +0300

    better solution added

--- src/code/SolveProblem.java
+++ src/code/SolveProblem.java
@@ -1,8 +1,15 @@
 package code;
 
+import java.util.HashMap;
+
 public class SolveProblem {
 
     public static void main(String[] args) {
+
+        String[] array1 = new String[]{"a", "b", "c", "d"};
+        String[] array2 = new String[]{"k", "l", "m"};
+
+        System.out.println(containCommonItem2(array1, array2));
 
     }
 
@@ -10,6 +17,7 @@ public class SolveProblem {
       brute force solution
       it is not the best because there is a nested loop
       so time complexity is O(m^n )
+      space complexity O(1)
       private boolean containCommonItem(String[] arr1, String[] arr2){
               for (String s : arr1) {
                   for (String value : arr2) {
@@ -23,8 +31,22 @@ public class SolveProblem {
 
     /**
      * better solution
+     * complexity O(m+n) if sizes are equal O(2n) -> O(n)
+     * space complexity O(m)
      */
-    private boolean containCommonItem2(String[] arr1, String[] arr2){
+    private static boolean containCommonItem2(String[] arr1, String[] arr2)
+    {
+        HashMap<String, Boolean> map = new HashMap<>();
+        if (arr1 != null && arr2 != null){
+            for (String s : arr1) {
+                if (!map.containsKey(s)) {
+                    map.put(s, true);
+                }
+            }
+            for (String s : arr2) {
+                if (map.containsKey(s)) return true;
+            }
+        }
         return false;
     }
 
